7bba9e399c86a8e90a599c51227661da3aa5ad3c
Yannick Lohse <glorf.travian@gmail.com>
2013-01-18 23:59:43 +0100

    Made closed class configurable

--- toggleable.jquery.js
+++ toggleable.jquery.js
@@ -1,15 +1,19 @@
 (function($){
-	$.fn.toggleable = function(){
+	$.fn.toggleable = function(options){
+		var settings = $.extend({
+			'closedClass': 'closed',
+		}, options);
+		
 		this.each(function(){
 			var $this = $(this);
 			var toggleSwitch = $this.find('.ux-toggle-switch');
 			var toggleContent = $this.find('.ux-toggle');
 			
-			$this.addClass('closed');
+			$this.addClass(settings.closedClass);
 			toggleContent.hide();
 			toggleSwitch.click(function(){
 				toggleContent.toggle();
-				$this.toggleClass('closed');
+				$this.toggleClass(settings.closedClass);
 			});
 		});
 		
